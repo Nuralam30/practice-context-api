63c2c61afd401da62d77649ced26cbb44e1b0627
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-18 12:37:54 +0600

    context api practice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Shipment from './components/Shipment/Shipment';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Shipment></Shipment>
+    </div>
+  );
+}
+
+export default App;
