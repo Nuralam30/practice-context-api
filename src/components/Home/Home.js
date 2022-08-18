24c9e6b7735425e93cd312b7bb8d93e948f4d26e
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-18 23:09:12 +0600

    state mangement for nested components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Categories from '../Categories/Categories';
 
 const Home = (props) => {
     const { count } = props;
     return (
-        <div>
+        <div style={{border: '3px solid red', padding: '20px'}}>
             <h1>This is home page : {count}</h1>
+            <Categories count={count}></Categories>
         </div>
     );
 };
